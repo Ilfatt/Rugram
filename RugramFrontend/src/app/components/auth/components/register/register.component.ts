42b3c75c773fc71b39b8f639c2941581d56f187d
Ilfatt <salavatyllinilfat@mail.ru>
2023-11-05 21:28:11 +0300

    UnionCommit
    q!
    
    edit .gitignore
    
    test
    
    test2
    
    test3
    
    test4

--- RugramFrontend/src/app/components/auth/components/register/register.component.ts
+++ RugramFrontend/src/app/components/auth/components/register/register.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: 'register.component.html',
+    styleUrls: ['register.component.css']
 })
-export class RegisterComponent {
+
+export class RegisterComponent implements OnInit {
+
+    constructor(public http: HttpClient) {
+    }
+
+    @Output()
+    onPressNext = new EventEmitter();
+    @Output()
+    onPressReturn = new EventEmitter();
+
+    showNext() {
+        const requestBody = {email: 'salavatyllinilfat@mail.ru'};
+        this.http.post('http://localhost:5062/auth/confirm-email', requestBody).subscribe();
+        this.onPressNext.emit();
+    }
+
+    showLogin() {
+        this.onPressReturn.emit();
+    }
+
+    ngOnInit(): void {
+
+    }
 
 }
+
