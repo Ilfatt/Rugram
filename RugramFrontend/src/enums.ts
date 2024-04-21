db5edc3ebb34026772e4c7294e7215240ff09323
Marsel <arknahon@gmail.com>
2024-04-21 17:27:49 +0300

    #2. Фикс картинок

--- RugramFrontend/src/enums.ts
+++ RugramFrontend/src/enums.ts
@@ -1,13 +1,13 @@
-import browse from "./assets/icons/Browse.svg";
-import search from "./assets/icons/Search.svg";
-import like from "./assets/icons/Like.svg";
-import profile from "./assets/icons/Profile.svg";
-import seeMore from "./assets/icons/SeeMore.svg";
-import plus from "./assets/icons/Plus.svg";
-import logo from "./assets/icons/logo.svg";
-import close from "./assets/icons/Close.svg";
-import exit from "./assets/icons/log-out.svg"
-import edit from "./assets/icons/edit.svg";
+import browse from "./assets/images/Browse.svg";
+import search from "./assets/images/Search.svg";
+import like from "./assets/images/Like.svg";
+import profile from "./assets/images/Profile.svg";
+import seeMore from "./assets/images/SeeMore.svg";
+import plus from "./assets/images/Plus.svg";
+import logo from "./assets/images/logo.svg";
+import close from "./assets/images/Close.svg";
+import exit from "./assets/images/log-out.svg"
+import edit from "./assets/images/edit.svg";
 
 export const icons = {
   browse,
